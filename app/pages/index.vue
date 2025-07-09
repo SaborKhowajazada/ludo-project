eb4ab6a70bd88de68aab3644f8e47f9f08bfcb4d
Sabor100 <sabor.khowajazada@gmail.com>
2025-07-09 21:09:15 +0200

    The second step is done now

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,39 @@
 <template>
-    <div>Hello world! where are you?</div>
+    <div class="highlight">Welcome to the Ludo game!</div>
+    <h2 class="text-h-two">How many players are there?</h2>
+  <form @submit.prevent="startTheGame" id="player-form">
+  <label><input type="radio" v-model="pick" name="players" value="2"> 2 player</label>
+  <label><input type="radio" v-model="pick" name="players" value="3"> 3 player</label>
+  <label><input type="radio" v-model="pick" name="players" value="4"> 4 player</label>
+
+  <button type="submit" class="space-between">Start the game</button>
+</form>
+
 </template>
+
+<script setup>
+const pick = ref("2")
+function startTheGame(){
+console.log("The game starts with ", pick.value, "people")
+}
+</script>
+
+
+<style>
+    .highlight {
+        text-align: center;
+        height: 70px;
+        margin-top: 60px;
+        color: rgba(255, 0, 0, 0.883);
+        font-weight: 700;
+    }
+    #player-form, .text-h-two {
+        text-align: center;
+        width: 100%;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 12px;
+    }
+</style>
